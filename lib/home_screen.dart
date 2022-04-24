de981c7a8a6ddd29d50b6399c13ee2c3ba158790
@ShiviCode <shiviyadav1999@gmail.com>
2022-04-25 00:48:30 +0530

    Add condition to show text when list is empty

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,7 +12,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     final task = Provider.of<ToDoModelProvider>(context, listen: true);
@@ -21,112 +20,119 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('Today\'s task to do'),
       ),
-      body: ListView.builder(
-          itemCount: task.taskList.length,
-          itemBuilder: (ctx, index) {
-            return Dismissible(
-              key: ValueKey(task.taskList[index].id),
-              background: Container(
-                color: Colors.red,
-                child: Icon(
-                  Icons.delete,
-                  color: Colors.white,
-                ),
-                alignment: Alignment.centerRight,
-                padding: EdgeInsets.only(right: 20),
-              ),
-              direction: DismissDirection.endToStart,
-              confirmDismiss: (direction) {
-                return showDialog(
-                    context: context,
-                    builder: (ctx) => AlertDialog(
-                          title: Text('are you sure?'),
-                          content: Text('Do you want to delete the task'),
-                          actions: [
-                            FlatButton(
-                                onPressed: () {
-                                  Navigator.of(ctx).pop(true);
-                                },
-                                child: Text('YES')),
-                            FlatButton(
-                                onPressed: () {
-                                  Navigator.of(ctx).pop(false);
-                                },
-                                child: Text('NO')),
-                          ],
-                        ));
-              },
-              onDismissed: (direction) {
-                Provider.of<ToDoModelProvider>(context, listen: false)
-                    .deleteProduct(task.taskList[index].id);
-              },
-              child: GestureDetector(
-                onTap: () {
-                  task.toggle(task.taskList[index].id);
-                },
-                child: Card(
-                  color: task.taskList[index].isChacked
-                      ? Color.fromARGB(255, 184, 255, 188)
-                      : Colors.white,
-                  child: ListTile(
-                    contentPadding: EdgeInsets.all(15),
-                    title: Text(
-                      '${task.taskList[index].title}',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      overflow: TextOverflow.ellipsis,
+      body: task.taskList.isEmpty
+          ? Center(
+              child: Text('Add New tasks...',style: TextStyle(fontSize: 20),),
+            )
+          : ListView.builder(
+              itemCount: task.taskList.length,
+              itemBuilder: (ctx, index) {
+                return Dismissible(
+                  key: ValueKey(task.taskList[index].id),
+                  background: Container(
+                    color: Colors.red,
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
                     ),
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.only(right: 20),
+                  ),
+                  direction: DismissDirection.endToStart,
+                  confirmDismiss: (direction) {
+                    return showDialog(
+                        context: context,
+                        builder: (ctx) => AlertDialog(
+                              title: Text('are you sure?'),
+                              content: Text('Do you want to delete the task'),
+                              actions: [
+                                FlatButton(
+                                    onPressed: () {
+                                      Navigator.of(ctx).pop(true);
+                                    },
+                                    child: Text('YES')),
+                                FlatButton(
+                                    onPressed: () {
+                                      Navigator.of(ctx).pop(false);
+                                    },
+                                    child: Text('NO')),
+                              ],
+                            ));
+                  },
+                  onDismissed: (direction) {
+                    Provider.of<ToDoModelProvider>(context, listen: false)
+                        .deleteProduct(task.taskList[index].id);
+                  },
+                  child: GestureDetector(
+                    onTap: () {
+                      task.toggle(task.taskList[index].id);
+                    },
+                    child: Card(
+                      color: task.taskList[index].isChacked
+                          ? Color.fromARGB(255, 184, 255, 188)
+                          : Colors.white,
+                      child: ListTile(
+                        contentPadding: EdgeInsets.all(15),
+                        title: Text(
+                          '${task.taskList[index].title}',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
 
-                    subtitle: Text(
-                      '${task.taskList[index].description}',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: Colors.black,
+                        subtitle: Text(
+                          '${task.taskList[index].description}',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        //leading: Icon(Icons.check_circle_outline),
+
+                        leading: task.taskList[index].isChacked
+                            ? Icon(
+                                Icons.check_circle,
+                                color: Colors.green,
+                              )
+                            : Icon(Icons.check_circle_outline),
+
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (ctx) => AlertDialog(
+                                      title: Text('are you sure?'),
+                                      content: Text(
+                                          'Do you want to delete the task'),
+                                      actions: [
+                                        FlatButton(
+                                            onPressed: () {
+                                              Provider.of<ToDoModelProvider>(
+                                                      context,
+                                                      listen: false)
+                                                  .deleteProduct(
+                                                      task.taskList[index].id);
+                                              Navigator.of(ctx).pop();
+                                            },
+                                            child: Text('YES')),
+                                        FlatButton(
+                                            onPressed: () {
+                                              Navigator.of(ctx).pop();
+                                            },
+                                            child: Text('NO')),
+                                      ],
+                                    ));
+                          },
+                        ),
                       ),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    //leading: Icon(Icons.check_circle_outline),
-
-                    leading: task.taskList[index].isChacked
-                        ? Icon(
-                            Icons.check_circle,
-                            color: Colors.green,
-                          )
-                        : Icon(Icons.check_circle_outline),
-
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        showDialog(
-                            context: context,
-                            builder: (ctx) => AlertDialog(
-                                  title: Text('are you sure?'),
-                                  content:
-                                      Text('Do you want to delete the task'),
-                                  actions: [
-                                    FlatButton(
-                                        onPressed: () {
-                                          Provider.of<ToDoModelProvider>(context, listen: false)
-                                          .deleteProduct(task.taskList[index].id);
-                                          Navigator.of(ctx).pop();
-                                        },
-                                        child: Text('YES')),
-                                    FlatButton(
-                                        onPressed: () {
-                                          Navigator.of(ctx).pop();
-                                        },
-                                        child: Text('NO')),
-                                  ],
-                                ));
-                      },
                     ),
                   ),
-                ),
-              ),
-            );
-          }),
+                );
+              }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
